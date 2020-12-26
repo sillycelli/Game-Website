e465e8d6cabafaf309e9d5b893c6b5302e7047fb
Aracelli Doescher <aracellidoescher@outlook.com>
2020-12-26 14:27:30 -0800

    added routing (sort of)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import {AboutUs} from './AboutUs';
+import {BrowserRouter, Link, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <nav className="nav">
+          <Link to="/Home">Home</Link>
+          <Link to="/AboutUs">About Us</Link>
+          <Link to="/PaiSho">Pai Sho</Link>
+        </nav>
+        <Switch>
+          <Route path="/AboutUs">
+            <AboutUs/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
