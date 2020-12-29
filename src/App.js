986b1c1b55fab9ef54bc0af78ea9f2acc9ef13a4
Alexander Harding Bradley <Alexanderhb02@gmail.com>
2020-12-29 20:13:45 +0100

    Fixed navigation bar, started login component

--- src/App.js
+++ src/App.js
@@ -6,25 +6,30 @@ import {BrowserRouter, Link, Route, Switch} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav';
+import { NavLink } from "react-router-dom";
 import { LinkContainer } from 'react-router-bootstrap';
 import { IndexLinkContainer } from 'react-router-bootstrap';
+import {Login} from './Login';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <h1>
-        <Navbar bg="dark" variant="dark">
+        <Navbar bg="dark" variant="dark" sticky="top">
           <Navbar.Collapse className="justify-content-center">
-            <Nav variant="tabs"  defaultActiveKey="/home">
-              <Nav.Link href="/home">Home</Nav.Link>
-              <Nav.Link href="/AboutUs">About Us</Nav.Link>
-              <Nav.Link href="/PaiSho">Pai Sho</Nav.Link>
+            <Nav variant="pills"  defaultActiveKey="/home">
+              <Nav.Link as ={NavLink} exact to="/home">Home</Nav.Link>
+              <Nav.Link as ={NavLink} exact to="/AboutUs">About Us</Nav.Link>
+              <Nav.Link as ={NavLink} exact to="/PaiSho">Pai Sho</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
       </h1>
         <Switch>
+          <Route path="/home">
+            <Login/>
+          </Route>
           <Route path="/AboutUs">
             <AboutUs/>
           </Route>
